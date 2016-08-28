443362461e8a608ba629ca972dd426ec3c7fd6a8
vv-boy <947704339@qq.com>
2016-08-28 18:14:18 +0800

    upload local file

---
+++ test/dealog2excel.php
@@ -0,0 +1,50 @@
+<?php
+
+require_once ("../vendor/autoload.php");
+
+// use PHPExcel\Spreadsheet;
+
+$objPHPExcel = new PHPExcel();
+$uuid = "test_uuid";
+$title = "$uuid click log";
+$objPHPExcel->getProperties()->setCreator($uuid)
+                             ->setTitle($title);
+
+/**
+ * 写头部信息。
+ * @var array
+ */
+$headerInfo = array("timestamp", "_type");
+
+$activeExcel = $objPHPExcel->setActiveSheetIndex(0);
+// var_dump($activeExcel);exit;
+foreach ($headerInfo as  $key=>$header) {
+	$activeExcel->setCellValue(chr($key + 65) . "1", $header);
+}
+
+
+/*
+写入excel
+ */
+
+$fp = fopen("../data/click_info.log", "r");
+$row = 2; // 从第二行开始写了。
+
+while (!feof($fp)) {
+	$clickInfo = fgets($fp);
+	if (!empty(trim($clickInfo))){
+		$clickArr = json_decode($clickInfo, true);
+		$i = 0;
+		foreach ($clickArr as $key => $info) {
+
+			$activeExcel->setCellValue(chr($i + 65) . strval($row), $info);
+			$i++;
+		}
+	}
+	$row ++;	
+}
+
+fclose($fp);
+
+$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
+$objWriter->save("../template/1.xlsx");
